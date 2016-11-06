291016cf69e427c52333e30a8f8f7757ab64eb8a
gillian420314 <gillian420314@gmail.com>
2016-11-06 22:03:39 +0800

    Update dot.ino

--- dot.ino
+++ dot.ino
@@ -12,11 +12,14 @@
 #define Light_PIN A0
 #define Button 2
 int buttonState = 0;
-int x = 140;
-int x2 = 140;
-int y = 22;
+int x1 = 128;
+int y1 = 22;
+int x2 = 26;
+int y2 = 22;
+int i = 0;
 int r;
 Adafruit_SSD1306 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
+
 static const unsigned char PROGMEM DOT[] = {
   0x3f,0x3f,
   0x7f,0xff,
@@ -31,47 +34,70 @@ static const unsigned char PROGMEM SQUARE[] = {
   0xff,0xff,
   0xff,0xff,
 };
-void square(){
-  display.drawBitmap(x, 22, SQUARE, 10, 5,WHITE);
-  display.display();
-  r = random(1, 8);
-  //delay(15);
-  //display.clearDisplay();
-}
-void dot(){
-  display.drawBitmap(25, y, DOT, 10, 5,WHITE);
-  display.display();
-  delay(15);
-  display.clearDisplay();
-  square();//call square
-  x-=r;
-}
-void jump(){
-   while(y>2){ 
-    dot();
-    y-=2;
-    Serial.println(x);
-   }
-   if(y<=2) y=2;
-   while(y<22){ 
-    dot(); 
-        y+=2; 
-        Serial.println(x); 
-      }
-}
+
 void gameover(){
   display.clearDisplay();
   display.setTextColor(WHITE);
-  display.setTextSize(3);
-  display.setCursor(20,15);
+  display.setTextSize(2);
+  display.setCursor(18,10);
   display.print("GAMEOVER");
   display.display();
 }
-void haha(){
+
+void youlose(){
+  delay(500);
   gameover();
+  //haha();
   while(1);
-  
-  }
+}
+void score(){
+  //display.clearDisplay();
+  display.setTextColor(WHITE);
+  display.setTextSize(1);
+  display.setCursor(60,0);
+  display.print("score:");
+  display.display();
+}
+void change(){
+  //display.clearDisplay();
+  display.setTextColor(WHITE);
+  display.setTextSize(1);
+  display.setCursor(100,0);
+  display.print(i);
+  display.display(); 
+}
+void square(){
+  display.drawBitmap(x1, y1, SQUARE, 10, 5,WHITE); 
+  display.display();
+  //delay(15);
+  //display.clearDisplay();
+}
+ 
+void dot(){
+  display.drawBitmap(x2, y2, DOT, 10, 5,WHITE);
+  display.display();
+  delay(15);
+  display.clearDisplay();
+  score();//call score
+  change();
+  square();//call square
+  x1-=r;
+}
+void jump(){
+   while(y2>2){ 
+    dot();
+    y2-=2;
+    Serial.println(x1);
+   }
+   if(y2<=2) y2=2;
+   while(y2<22){ 
+    dot(); 
+        y2+=2; 
+        Serial.println(x1);
+      }
+
+}
+
 void setup() {
   Serial.begin(9600);
   display.begin(SSD1306_SWITCHCAPVCC);
@@ -80,14 +106,17 @@ void setup() {
 }
 
 void loop() {
-  if(x==y) haha();
+  //change();
   buttonState = digitalRead(Button);
   if ( buttonState == HIGH ){ jump(); }
-  dot(); Serial.println(x); 
+  dot();  Serial.println(x1); 
+  r = random(1, 8);
+  if(x1<=0) {x1=128;}
+  if(x1<=-30) {i++;change();}
   
-  if(x<=0) x=140;
+  if ( (x1>24 && x1<32) && y2>18) youlose();
   
-   //while(x=140){quare();}
-   //if (x2<=r) {x2=140;}
-   //if() gameover();
+  //if ( (x1>24 && x1<32) ) youlose();
+  //if ( x1>26 && x1<30 ) youlose();
+  if ( y2>18 && y2<22 ) youlose(); 
 }
